5dd086c70f64d202b91d3fe9611971bf9a56a9b6
FranciscoEstrada02 <francisco.s295973@cesurformacion.com>
2023-01-19 10:15:30 +0100

    primer commit

---
+++ Usuario/src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese su nombre de usuario para registrarse");
+        String user = sc.nextLine();
+        System.out.println("Ingrese su contraseña para registrarse");
+        String constraseña = sc.nextLine();
+        Usuario usuario = new Usuario(user, constraseña);
+        for (int i = 0; i < 4; i++) {
+            System.out.println("Ingrese si desea logearse o no");
+            String login = sc.nextLine();
+            if (login.equals("si")) {
+                System.out.println("Ingrese su usuario");
+                String usuario1 = sc.nextLine();
+                System.out.println("Ingrese su contraseña");
+                String contraseña1 = sc.nextLine();
+                System.out.println(usuario.login(usuario1, contraseña1, usuario));
+            } else if (login.equals("no")) {
+                if (usuario.isEstadoUsuario() == false) {
+                    System.out.println("Ya esta desconectado");
+                } else {
+                    System.out.println("DESCONECTADO");
+                }
+            }
+        }
+    }
+}
---
+++ Usuario/src/Usuario.java
@@ -0,0 +1,65 @@
+public class Usuario {
+    private String user;
+    private String password;
+
+    public boolean estadoUsuario;
+    private Usuario usuario;
+
+    public Usuario(String user, String password) {
+        this.user = user;
+        this.password = password;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isEstadoUsuario() {
+        return estadoUsuario;
+    }
+
+    public void setEstadoUsuario(boolean estadoUsuario) {
+        this.estadoUsuario = estadoUsuario;
+    }
+
+    public String covertirContraseña() {
+        String cadenaNueva = "";
+        for (int i = 0; i < this.password.length(); i++) {
+            cadenaNueva = cadenaNueva + "*";
+        }
+        return cadenaNueva;
+    }
+
+    public String login(String usuario1, String contraseña1, Usuario usuario) {
+        if (this.estadoUsuario) return "YA ESTA CONECTADO";
+
+        if ((usuario1.equals(usuario.getUser())) && (contraseña1.equals(usuario.getPassword()))) {
+            estadoUsuario = true;
+            return "CONEXION ESTABLECIDA";
+        }
+        return "NO SE PUDO CONECTAR";
+    }
+
+    public boolean conexion(boolean estadoUsuario) {
+
+        if (estadoUsuario) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+
+}
