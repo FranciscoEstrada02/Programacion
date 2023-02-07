751d2d77c78e8b4a058872aab4c43fd4731d54ff
FranciscoEstrada02 <francisco.s295973@cesurformacion.com>
2023-02-07 23:23:30 +0100

    Commit

--- Cuenta/src/Cuenta.java
+++ Cuenta/src/Cuenta.java
@@ -2,7 +2,7 @@ public class Cuenta {
     private String cliente;
     private String numCuenta;
     private double interes;
-    private double saldo;
+    public double saldo;
 
     public Cuenta() {
         this.cliente = cliente;
@@ -17,11 +17,12 @@ public class Cuenta {
         this.interes = interes;
         this.saldo = saldo;
     }
-    public Cuenta(String cliente, String numCuenta, double interes, double saldo,Cuenta cuenta) {
-        this.cliente = cliente;
-        this.numCuenta = numCuenta;
-        this.interes = interes;
-        this.saldo = saldo;
+    public Cuenta(Cuenta cuentaCopia) {
+        this.cliente = cuentaCopia.cliente;
+        this.numCuenta = cuentaCopia.numCuenta;
+        this.interes = cuentaCopia.interes;
+        this.saldo = cuentaCopia.saldo;
+
 
     }
 
@@ -55,5 +56,43 @@ public class Cuenta {
 
     public void setSaldo(double saldo) {
         this.saldo = saldo;
+    }
+
+    public boolean ingreso(double cantidad ){
+        if(cantidad < 0){
+            return false;
+        }else{
+            saldo += cantidad;
+            return true;
+        }
+
+    }
+
+    public boolean reintegro(int cantidad){
+        if((cantidad > saldo) && (cantidad <= 0 )){
+            saldo = saldo;
+            return false;
+        }else{
+            saldo -= cantidad;
+            return true;
+        }
+    }
+
+    public boolean transferencia(Cuenta cuenta, double importe){
+        if(importe > saldo){
+            return false;
+        }else{
+            saldo -= importe;
+            cuenta.saldo += importe;
+            return true;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Titular: " + this.cliente +
+                "\nNumero de Cuenta: " + this.numCuenta +
+                "\nIntereses: " + this.interes +
+                "\nSaldo: " + this.saldo;
     }
 }
--- Cuenta/src/Main.java
+++ Cuenta/src/Main.java
@@ -1,6 +1,77 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean opcion1 = true;
+        Cuenta cuenta1 = null;
+        Cuenta cuentaDestinoo = new Cuenta("Antonio", "23123", 2, 100);
 
+        while (opcion1) {
+            System.out.println("Que desea hacer:" +
+                    "\n1. Crear Cuenta" +
+                    "\n2. Mostrar saldo cuenta" +
+                    "\n3. Hacer ingreso cuenta" +
+                    "\n4. Hacer transferencia" +
+                    "\n5. Mostrar datos de la cuenta" +
+                    "\n6. Salir");
 
+            int opcion = sc.nextInt();
+
+            sc.nextLine();
+
+            if (opcion == 1) {
+                System.out.println("Ingrese su usuario");
+                String usuario = sc.nextLine();
+                System.out.println("Ingrese su numero de cuenta");
+                String numCuenta = sc.nextLine();
+                System.out.println("Ingrese su saldo");
+                int saldo = sc.nextInt();
+                cuenta1 = new Cuenta(usuario, numCuenta, 2, saldo);
+
+            } else if (opcion == 2) {
+                if (cuenta1 == null) {
+                    System.out.println("No hay ninguna cuenta creada");
+                } else {
+                    System.out.println("El saldo de la cuenta es: " + cuenta1.getSaldo());
+                }
+            } else if (opcion == 3) {
+                if (cuenta1 == null) {
+                    System.out.println("No hay ninguna cuenta creada");
+                } else {
+                    System.out.println("Cuanto quiere ingresar");
+                    double cantidadIngresar = sc.nextInt();
+                    if (cuenta1.ingreso(cantidadIngresar)) {
+                        System.out.println("Ingreso realizado con exito");
+                    } else {
+                        System.out.println("La cantidad a ingresar no puede ser negativa");
+                    }
+                }
+            } else if (opcion == 4) {
+                double cantidad = 0;
+                if (cuenta1 == null) {
+                    System.out.println("No hay ninguna cuenta creada");
+                } else {
+                    System.out.print("Introduce la cuenta destino: ");
+                    String cuentaDestino = sc.nextLine();
+                    System.out.print("Introduce la cantidad a transferir: ");
+                    cantidad = sc.nextDouble();
+                    if (cuenta1.transferencia(cuentaDestinoo, cantidad)) {
+                        System.out.println("Tranferencia realizada con exito");
+                    } else {
+                        System.out.println("No se puedo realizar la tranferecia");
+                    }
+                }
+
+            } else if (opcion == 5) {
+                if (cuenta1 == null) {
+                    System.out.println("No hay ninguna cuenta creada");
+                } else {
+                    cuenta1.toString();
+                }
+            } else if (opcion == 6) {
+                opcion1 = false;
+            }
+        }
     }
 }
