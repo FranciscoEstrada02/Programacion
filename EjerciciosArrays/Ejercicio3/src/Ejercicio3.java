64f4267fd1979d93b678ee8c2342466e32dd4268
FranciscoEstrada02 <francisco.s295973@cesurformacion.com>
2023-02-23 09:13:04 +0100

    Ejercicio 3

--- EjerciciosArrays/Ejercicio3/src/Ejercicio3.java
+++ EjerciciosArrays/Ejercicio3/src/Ejercicio3.java
@@ -1,7 +1,5 @@
-import java.util.Scanner;
-
 public class Ejercicio3 {
-    private char[] abecedario = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','W','X','Y','Z'};
+    private char[] abecedario = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'W', 'X', 'Y', 'Z'};
 
     public Ejercicio3() {
         this.abecedario = abecedario;
@@ -15,24 +13,13 @@ public class Ejercicio3 {
         this.abecedario = abecedario;
     }
 
-    public void hacerCadena(){
-        Boolean condicion = true;
+    public String hacerCadena(int valor) {
         String cadena = "";
-        Scanner sc = new Scanner(System.in);
-        while(condicion){
-            System.out.println("Ingrese un valor");
-            int valor = sc.nextInt();
-            if(valor == -1){
-                System.out.println("PROCESO TERMINADO");
-                condicion = false;
-
-            }else if(valor > 24 || valor < 0){
-                System.out.println("VALOR NO VALIDO, INGRESE OTRO");
-            }else {
-                cadena = cadena + String.format(String.valueOf(abecedario[valor]));
-                System.out.println(cadena);
-            }
-
+        if (valor > 24 || valor < 0) {
+            System.out.println("VALOR NO VALIDO, INGRESE OTRO");
+        } else {
+            cadena = cadena + String.format(String.valueOf(abecedario[valor]));
         }
+        return cadena;
     }
 }
--- EjerciciosArrays/Ejercicio3/src/Main.java
+++ EjerciciosArrays/Ejercicio3/src/Main.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
         Ejercicio3 ejercicio3 = new Ejercicio3();
-        ejercicio3.hacerCadena();
+        Boolean condicion = true;
+        String cadena = "";
+
+        while (condicion) {
+            System.out.println("Ingrese un valor");
+            int valor = sc.nextInt();
+            if (valor == -1) {
+                System.out.println("PROCESO TERMINADO");
+                condicion = false;
+            }  else {
+                cadena = cadena + ejercicio3.hacerCadena(valor);
+                System.out.println(cadena);
+            }
+
+        }
     }
 }
